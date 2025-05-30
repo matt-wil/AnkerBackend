ae21c92044b31924728659911a83ffefa183f5b6
Matt Williams <matthew.steven.williams@gmail.com>
2025-05-30 17:52:04 +0200

    change req.txt

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ def create_app():
     # config
     app = Flask(__name__)
     app.config.from_object(Config)
-    CORS(app, origins="http://localhost:5173")
+    CORS(app, origins=["http://localhost:5173"])
 
     # initialization
     db.init_app(app)
