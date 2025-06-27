eb33bc24e4155f95dded98bf184924e61b9327b4
Matt Williams <matthew.steven.williams@gmail.com>
2025-06-27 13:09:41 +0200

    add deployed frontend for cors

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ def create_app():
     # config
     app = Flask(__name__)
     app.config.from_object(Config)
-    CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
+    CORS(app, supports_credentials=True, origins=["http://localhost:5173", "https://anker-tattoo-piercing.vercel.app"])
 
     # initialization
     db.init_app(app)
